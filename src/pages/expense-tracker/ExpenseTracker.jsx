615a1e4c8c2809e97db42e2231b475c2abd5906e
Kuldeep rathor <127830786+kuldeep-rathor@users.noreply.github.com>
2023-09-23 10:48:03 +0530

    email-verification

--- src/pages/expense-tracker/ExpenseTracker.jsx
+++ src/pages/expense-tracker/ExpenseTracker.jsx
@@ -1,31 +1,36 @@
 import React from "react";
 
 const ExpenseTracker = () => {
-  return <div className="expense-tracker">
-    <div className="container">
-
-      <h1>Expense Tracker</h1>
-      <div className="balance">
-        <h3>Your Balance</h3>
-        <h2>$0.00</h2>
-      </div>
-      <div className="summary">
-        <div className="income">
-          <h4>Income</h4>
-          <p>$0.00</p>
+  return (
+    <div className="expense-tracker">
+      <div className="container">
+        <h1>Expense Tracker</h1>
+        <div className="balance">
+          <h3>Your Balance</h3>
+          <h2>$0.00</h2>
         </div>
-        <div className="expenses">
-          <h4>Expenses</h4>
-          <p>$0.00</p>
+        <div className="summary">
+          <div className="income">
+            <h4>Income</h4>
+            <p>$0.00</p>
+          </div>
+          <div className="expenses">
+            <h4>Expenses</h4>
+            <p>$0.00</p>
+          </div>
         </div>
+        <form className="add-transaction">
+          <input type="text" placeholder="Description" required />
+          <input type="number" placeholder="Amount" required />
+          <label htmlFor="expense">Expense</label>
+          <input type="radio" id="expense" value="expense" />
+          <label htmlFor="income">Income</label>
+          <input type="radio" id="income" value="income" />
+          <button type="submit"> Add Transaction</button>
+        </form>
       </div>
-      <form  className="add-transaction">
-        <input type="text" placeholder="Description" required />
-        <input type="number" placeholder="Amount" required />
-        
-      </form>
     </div>
-  </div>;
+  );
 };
 
 export default ExpenseTracker;
